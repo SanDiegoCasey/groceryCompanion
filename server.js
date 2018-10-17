9d0850a222658248b526cf764cd1fbb7c0ec7c8f
Casey Whitcher <sandiegocasey@gmail.com>
2018-10-17 18:04:09 -0500

    nodemon, public, config

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise; //set mongoose promise style to es6
+
+const { DATABASE_URL, PORT } = require("./config");
+const { ModelLink1, ModelLink2 } = require("./models");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(morgan("common"));
+app.use(express.json());
