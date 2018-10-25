cc068eb79f25266c307291d19054b372d6068a00
Casey Whitcher <sandiegocasey@gmail.com>
2018-10-24 19:55:00 -0500

    added error handling on front page

--- app.js
+++ app.js
@@ -8,18 +8,35 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const storeRoutes = require('./api/routes/stores');
 
+app.use(morgan('common'));
+
 app.use('/products', productRoutes);
 app.use('/stores', storeRoutes);
 
+app.use(express.static('public/views'));
+app.use(express.json());
 
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 
 mongoose.Promise = global.Promise; //set mongoose promise style to es6
 
 
-app.use(morgan('common'));
 
-app.use(express.static('public/views'));
-app.use(express.json());
+
+
 
 module.exports = app;
